77a495e5d1098ffa3b9fe772c858f8aca23a3529
Ariel Hurtado <arielhurtado388@gmail.com>
2025-12-18 17:12:34 -0500

    Testing a obtener, editar, y eliminar por id

---
+++ src/tests/mocks/gastos.ts
@@ -0,0 +1,26 @@
+export const gastos = [
+  {
+    id: 1,
+    nombre: "Limosina",
+    cantidad: 300,
+    idPresupuesto: 1,
+    createdAt: "2030-06-17T17:20:57.138Z",
+    updatedAt: "2030-06-17T17:20:57.138Z",
+  },
+  {
+    id: 2,
+    nombre: "Fotos",
+    cantidad: 400,
+    idPresupuesto: 1,
+    createdAt: "2030-06-17T17:21:03.077Z",
+    updatedAt: "2030-06-17T17:21:03.077Z",
+  },
+  {
+    id: 3,
+    nombre: "Viaje",
+    cantidad: 200,
+    idPresupuesto: 2,
+    createdAt: "2030-06-17T17:21:07.854Z",
+    updatedAt: "2030-06-17T17:21:07.854Z",
+  },
+];
--- src/tests/unit/controllers/GastoController.test.ts
+++ src/tests/unit/controllers/GastoController.test.ts
@@ -1,6 +1,7 @@
 import { createRequest, createResponse } from "node-mocks-http";
 import Gasto from "../../../models/Gasto";
 import { GastoController } from "../../../controllers/GastoController";
+import { gastos } from "../../mocks/gastos";
 
 jest.mock("../../../models/Gasto", () => ({
   create: jest.fn(),
@@ -68,5 +69,80 @@ describe("GastoController.crear", () => {
     expect(data).toEqual({ error: "Hubo un error" });
     expect(gastoMock.save).not.toHaveBeenCalled();
     expect(Gasto.create).toHaveBeenCalledWith(req.body);
+  });
+});
+
+describe("GastoController.obtenerPorId", () => {
+  it("Debe devolver todos los gastos del presupuesto con id: 1", async () => {
+    const req = createRequest({
+      method: "GET",
+      url: "/api/presupuestos/:idPresupuesto/gastos/:idGasto",
+      gasto: gastos[0],
+    });
+
+    const res = createResponse();
+
+    await GastoController.obtenerPorId(req, res);
+
+    const data = res._getJSONData();
+
+    expect(res.statusCode).toBe(200);
+    expect(data).toEqual(gastos[0]);
+  });
+});
+
+describe("GastoController.editarPorId", () => {
+  it("Debe manejar la actualizacion de un gasto y regresar un mensaje de exito", async () => {
+    const gastoMock = {
+      ...gastos[0],
+      update: jest.fn(),
+    };
+
+    const req = createRequest({
+      method: "PUT",
+      url: "/api/presupuestos/:idPresupuesto/gastos/:idGasto",
+      gasto: gastoMock,
+      body: {
+        nombre: "Gasto Actualizado",
+        cantidad: 100,
+      },
+    });
+
+    const res = createResponse();
+
+    await GastoController.editarPorId(req, res);
+
+    const data = res._getJSONData();
+
+    expect(res.statusCode).toBe(200);
+    expect(data).toBe("Gasto actualizado correctamente");
+    expect(gastoMock.update).toHaveBeenCalledWith(req.body);
+    expect(gastoMock.update).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe("GastoController.eliminarPorId", () => {
+  it("Debe manejar la eliminacion de un gasto y regresar un mensaje de exito", async () => {
+    const gastoMock = {
+      ...gastos[0],
+      destroy: jest.fn(),
+    };
+
+    const req = createRequest({
+      method: "DELETE",
+      url: "/api/presupuestos/:idPresupuesto/gastos/:idGasto",
+      gasto: gastoMock,
+    });
+
+    const res = createResponse();
+
+    await GastoController.eliminarPorId(req, res);
+
+    const data = res._getJSONData();
+
+    expect(res.statusCode).toBe(200);
+    expect(data).toBe("Gasto eliminado correctamente");
+    expect(gastoMock.destroy).toHaveBeenCalled();
+    expect(gastoMock.destroy).toHaveBeenCalledTimes(1);
   });
 });
