34eaa3bd4121e097f645a7b2273dcaed38adf17d
Ariel Hurtado <arielhurtado388@gmail.com>
2025-12-18 23:36:32 -0500

    Testing a inicio de sesion

--- src/tests/unit/controllers/AuthController.test.ts
+++ src/tests/unit/controllers/AuthController.test.ts
@@ -1,13 +1,15 @@
 import { createRequest, createResponse } from "node-mocks-http";
 import { AuthController } from "../../../controllers/AuthController";
 import Usuario from "../../../models/Usuario";
-import { hashPassword } from "../../../helpers/auth";
+import { hashPassword, verificarPassword } from "../../../helpers/auth";
 import { generarToken } from "../../../helpers/token";
 import { AuthCorreo } from "../../../correos/AuthCorreo";
+import { generarJWT } from "../../../helpers/jwt";
 
 jest.mock("../../../models/Usuario");
 jest.mock("../../../helpers/auth");
 jest.mock("../../../helpers/token");
+jest.mock("../../../helpers/jwt");
 
 describe("AuthController.crearCuenta", () => {
   beforeEach(() => {
@@ -76,5 +78,120 @@ describe("AuthController.crearCuenta", () => {
     });
     expect(AuthCorreo.enviarConfirmacion).toHaveBeenCalledTimes(1);
     expect(res.statusCode).toBe(201);
+  });
+});
+
+describe("AuthController.iniciarSesion", () => {
+  it("Debe manejar cuando un usuario no existe", async () => {
+    (Usuario.findOne as jest.Mock).mockResolvedValue(null);
+
+    const req = createRequest({
+      method: "POST",
+      url: "/api/aut/iniciar-sesion",
+      body: {
+        correo: "prueba@prueba.com",
+        password: "testpassword",
+      },
+    });
+
+    const res = createResponse();
+    await AuthController.iniciarSesion(req, res);
+
+    const data = res._getJSONData();
+    expect(res.statusCode).toBe(404);
+    expect(data).toEqual({ error: "Usuario no encontrado" });
+  });
+
+  it("Debe manejar cuando un usuario existe pero no esta confirmado", async () => {
+    (Usuario.findOne as jest.Mock).mockResolvedValue({
+      id: 1,
+      correo: "test@test.com",
+      password: "password",
+      confirmado: false,
+    });
+
+    const req = createRequest({
+      method: "POST",
+      url: "/api/aut/iniciar-sesion",
+      body: {
+        correo: "prueba@prueba.com",
+        password: "testpassword",
+      },
+    });
+
+    const res = createResponse();
+    await AuthController.iniciarSesion(req, res);
+
+    const data = res._getJSONData();
+    expect(res.statusCode).toBe(403);
+    expect(data).toEqual({ error: "Cuenta no confirmada" });
+  });
+
+  it("Debe manejar si el password es correcto", async () => {
+    const usuarioMock = {
+      id: 1,
+      correo: "test@test.com",
+      password: "password",
+      confirmado: true,
+    };
+
+    (Usuario.findOne as jest.Mock).mockResolvedValue(usuarioMock);
+
+    const req = createRequest({
+      method: "POST",
+      url: "/api/aut/iniciar-sesion",
+      body: {
+        correo: "prueba@prueba.com",
+        password: "testpassword",
+      },
+    });
+
+    const res = createResponse();
+    (verificarPassword as jest.Mock).mockResolvedValue(false);
+
+    await AuthController.iniciarSesion(req, res);
+
+    const data = res._getJSONData();
+    expect(res.statusCode).toBe(401);
+    expect(data).toEqual({ error: "Contraseña incorrecta" });
+    expect(verificarPassword).toHaveBeenCalledWith(
+      req.body.password,
+      usuarioMock.password
+    );
+    expect(verificarPassword).toHaveBeenCalledTimes(1);
+  });
+
+  it("Debe manejar la generacion del JWT", async () => {
+    const usuarioMock = {
+      id: 1,
+      correo: "test@test.com",
+      password: "hashedpassword",
+      confirmado: true,
+    };
+
+    const req = createRequest({
+      method: "POST",
+      url: "/api/aut/iniciar-sesion",
+      body: {
+        correo: "test@test.com",
+        password: "password",
+      },
+    });
+
+    const res = createResponse();
+
+    const fakeJWT = "fakejwt";
+
+    (Usuario.findOne as jest.Mock).mockResolvedValue(usuarioMock);
+    (verificarPassword as jest.Mock).mockResolvedValue(true);
+    (generarJWT as jest.Mock).mockReturnValue(fakeJWT);
+
+    await AuthController.iniciarSesion(req, res);
+
+    const data = res._getJSONData();
+    expect(res.statusCode).toBe(200);
+    expect(data).toEqual(fakeJWT);
+    expect(generarJWT).toHaveBeenCalledTimes(1);
+    expect(generarJWT).toHaveBeenCalledWith(usuarioMock.id);
   });
 });
