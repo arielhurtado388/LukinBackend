020a6b00a35ce13283ae183b25e9f8067be7c739
Ariel Hurtado <arielhurtado388@gmail.com>
2025-12-23 18:22:42 -0500

    Url del front en correos

--- src/correos/AuthCorreo.ts
+++ src/correos/AuthCorreo.ts
@@ -15,7 +15,7 @@ export class AuthCorreo {
       html: `
         <p>Hola ${usuario.nombre}, has creado tu cuenta en Lukin y debes confirmarla.</p>
         <p>Visita el siguiente enlace: </p>
-        <a href="#">Confirmar cuenta</a>
+        <a href="${process.env.FRONTEND_URL}/auth/confirmar">Confirmar cuenta</a>
         <p>e ingresa el código: <b>${usuario.token}</b></p>
       `,
     });
@@ -30,7 +30,7 @@ export class AuthCorreo {
       html: `
         <p>Hola ${usuario.nombre}, has solicitado reestablecer tu contraseña en Lukin.</p>
         <p>Visita el siguiente enlace: </p>
-        <a href="#">Reestablecer contraseña</a>
+        <a href="${process.env.FRONTEND_URL}/auth/reestablecer">Reestablecer contraseña</a>
         <p>e ingresa el código: <b>${usuario.token}</b></p>
       `,
     });
